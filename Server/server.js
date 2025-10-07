7385ed0663dc95e2493e881f6082e9642b862dc9
Mohamad Yaseen <mohamadyaseen233@gmail.com>
2025-10-07 06:17:44 +0100

    Chnged email provider

--- Server/server.js
+++ Server/server.js
@@ -83,9 +83,7 @@ app.post("/api/enquiry", async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+        service: "gmail",
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
